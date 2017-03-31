21522e6f0d79a38386778f151c435cb2ff9313d2
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2017-03-31 09:47:49 +0700

    pussh head and display show hide menu fixed

--- modul/master_kategori_layanan.php
+++ modul/master_kategori_layanan.php
@@ -6,22 +6,14 @@
     	});
 	});
 </script>
-<div class="row">
-	<div class='main-containpages'>
-		<div class="heading-menubar"><h3>Master Kategori Layanan</h3></div>
-			<button class="btn-cliked" style="margin-bottom:20px;">Tambah Kategori Layanan</button>
+<div class="heading-menubar"><h3>Master Kategori Layanan</h3></div>
+	<button class="btn-cliked">Tambah Kategori Layanan</button>
+	<div class="row">
+		<div class='main-containpages'>
 			<div class="btn-show" style="display:none;">
 				<div class="col-md-6">
 					<form action="backend/proses_master_kategori_layanan.php?act=add_kategori_layanan" method="post" id="" enctype="multipart/form-data">
-						<!-- <div class="form-group">
-							<label>Kode layanan</label>
-							<select name="kode_layanan" class="form-control" autofocus required="">
-								<option value="CLN">CLN</option> 
-								<option value="RPNT">RPNT</option> 
-								<option value="RGL">RGL</option> 
-							</select>
-						</div> -->
-						<div class="form-group">
+						<div class="form-group" style="margin-top:30px;">
 							<label>Jenis layanan</label>
 							<input type="text" name="jenis_layanan" class="form-control" autofocus required="">
 						</div>
@@ -44,47 +36,48 @@
 				</div><!--col-md-6-->
 				<div class="col-md-6">
 					<div class="panel panel-default">
-						<div class="panel-heading" style="background-color:#212121;color:#fff;">Keterangan Kode :</div> 
+						<div class="panel-heading" style="background-color:#212121;color:#fff;">Keterangan :</div> 
 						<div class="panel-body">
-							<p>CLN<span>: Adalah jenis layanan Cleaner ( jasa layanan pencucian sepatu )</span></p>
-							<p>RPT<span>: Adalah jenis layanan Repaint ( jasa layanan pewaranaan sepatu )</span></p>
-							<p>RGL<span>: Adalah jenis layanan Reglue ( jasa layanan pengeleman sol sepatu )</span></p>
+							<p>Cleaning<span>: Adalah jenis layanan Cleaner ( jasa layanan pencucian sepatu )</span></p>
+							<p>Repaint<span>: Adalah jenis layanan Repaint ( jasa layanan pewaranaan sepatu )</span></p>
+							<p>Reglue<span>: Adalah jenis layanan Reglue ( jasa layanan pengeleman sol sepatu )</span></p>
 						</div>
 					</div>
 				</div>
 			</div><!-- div display none -->
-		<div class="col-lg-12 clearfix-bottom clearfix-upertop">
-			<table class="table table-hover" id="table-master-kate-layanan">
-				<thead class="custom-headtables-globalconf">
-					<tr>
-						<th>No</th>
-						<th>Kategori layanan</th>
-						<th>Nama layanan</th>
-						<th>Harga layanan</th>
-						<th>Deskripsi layanan</th>
-						<th>Action</th>
-					</tr>
-				</thead>	
-				<tbody>
-					<?php
-						$no=1;
-						$x = mysqli_query($con,"SELECT * FROM kategori_layanan ORDER BY jenis_layanan ASC");
-						while ($res=mysqli_fetch_array($x)) {
-					 ?>
-					<tr>
-						<td><?php echo $no;?></td>
-						<td><?php echo $res['jenis_layanan']; ?></td>
-						<td><?php echo $res['nama_layanan']; ?></td>
-						<td>Rp.<?php echo formatuang($res['harga_layanan']); ?></td>
-						<td><?php echo $res['deskripsi_layanan']; ?></td>
-						<td>
-							<a href="homeadmin.php?page=master_edit_kategory_layanan&id=<?php echo $res['id_kategori_layanan']?>">Edit</a> ||
-							<a onclick="return confirm('Anda yakin menghapus !!')" href="backend/proses_master_kategori_layanan.php?act=delete_kate_layanan&id=<?php echo $res['id_kategori_layanan'];?>">Hapus</a>
-						</td>
-					</tr>
-					<?php $no++; } ?>
-				</tbody>					
-			</table>
+			<div class="col-lg-12 clearfix-bottom clearfix-upertop">
+				<table class="table table-hover" id="table-master-kate-layanan">
+					<thead class="custom-headtables-globalconf">
+						<tr>
+							<th>No</th>
+							<th>Kategori layanan</th>
+							<th>Nama layanan</th>
+							<th>Harga layanan</th>
+							<th>Deskripsi layanan</th>
+							<th>Action</th>
+						</tr>
+					</thead>	
+					<tbody>
+						<?php
+							$no=1;
+							$x = mysqli_query($con,"SELECT * FROM kategori_layanan ORDER BY jenis_layanan ASC");
+							while ($res=mysqli_fetch_array($x)) {
+						 ?>
+						<tr>
+							<td><?php echo $no;?></td>
+							<td><?php echo $res['jenis_layanan']; ?></td>
+							<td><?php echo $res['nama_layanan']; ?></td>
+							<td>Rp.<?php echo formatuang($res['harga_layanan']); ?></td>
+							<td><?php echo $res['deskripsi_layanan']; ?></td>
+							<td>
+								<a href="homeadmin.php?page=master_edit_kategory_layanan&id=<?php echo $res['id_kategori_layanan']?>">Edit</a> ||
+								<a onclick="return confirm('Anda yakin menghapus !!')" href="backend/proses_master_kategori_layanan.php?act=delete_kate_layanan&id=<?php echo $res['id_kategori_layanan'];?>">Hapus</a>
+							</td>
+						</tr>
+						<?php $no++; } ?>
+					</tbody>					
+				</table>
+			</div>
 		</div>
 	</div><!--maincontain-page-->
 </div><!--row-->
