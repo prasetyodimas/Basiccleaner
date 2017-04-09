949849729d7a518b0f3819708776f6aa69d249b2
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2017-04-10 00:00:28 +0700

    tambah keterangan pusat bantuan

--- modul/setting.php
+++ modul/setting.php
@@ -88,10 +88,10 @@
                 <div class="row">
                     <div class="panel-body show-content3" style="display:none;">
                         <div class="col-lg-12 custom-keterangan-pad">
-                            <p>1. Langkah pertama klik pada menu setting pengguna</p>
-                            <p>2. Kemudian akan muncul data pengguna sistem, jika ingin menambah user baru atau mengelola isikan pada kolom tambah pengguna, 
-                            jika ingin mengelola hak akses atau memblokir hak pengguna cukup klik pada menu edit kemudian ubah hakases status penggunanya untuk
-                            lebih jelas lihat pada gambar dibawah ini </p>
+                            <p>1. Langkah pertama klik pada menu transaksi, kemudia pilih menu transaksi keluar, transaksi keluar ini adalah proses sebuah transaksi
+                            yang telah di lakukan oleh seorang pelanggan atau <i>customer</i>.</p>
+                            <p>2. Pengertian transaksi keluar di sini adalah setiap transaksi yang masuk pada hari apapun masuk kedalam list transaksi keluar,
+                            kesimpulan nya adalah sebuah aktivitas yang di telah dilakukan.</p>
                         </div>
                         <div class="col-lg-12">
                             <img src="<?php echo $site;?>frontend/pusat_bantuan/list transaksi keluar.png" class="img-responsive">
